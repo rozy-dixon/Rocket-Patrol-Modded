fbb27f31b33ba801165b59de56f6f81c8a7cc7e5
Rosalind Dixon <romdixon@ucsc.edu>
2024-01-27 17:26:48 -0800

    removing explosion animation references (warnings)

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,13 +9,6 @@ class Menu extends Phaser.Scene {
         this.load.image('spaceship', './assets/spaceship.png')
         this.load.image('spaceshuttle', './assets/spaceshuttle.png')
         this.load.image('starfield', './assets/starfield.png')
-        // load spritesheet
-        this.load.spritesheet('explosion', './assets/explosion.png', {
-            frameWidth: 64,
-            frameHeight: 32,
-            startFrame: 0,
-            endFrame: 9
-        })
         // load audio
         this.load.audio('sfx-select', './assets/sfx-select.wav')
         this.load.audio('sfx-explosion1', './assets/sfx-explosion1.wav')
@@ -29,13 +22,6 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        // animation config
-        this.anims.create({
-            key: 'explode',
-            frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
-            frameRate: 30
-        })
-
         let menuConfig = {
             fontFamily: 'Courier',
             fontSize: '28px',
